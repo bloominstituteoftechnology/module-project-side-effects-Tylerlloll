311ab797885039b84ed304399f9df7d7c8fbd71d
Tyler Leach <tylerleach2918@gmail.com>
2024-06-27 08:32:19 -0700

    finished

--- frontend/components/Posts/LikeSection.js
+++ frontend/components/Posts/LikeSection.js
@@ -3,6 +3,7 @@
 // You will also add an onClick handler that utilizes `likePost` to increase the count of likes.
 // (As a stretch goal, you might want to prevent your user from "liking" the same post more than once.)
 import React from 'react';
+import { number } from 'yup';
 
 const LikeSection = props => {
   // 🔥 Make sure the parent of LikeSection is passing the right props!
@@ -14,14 +15,14 @@ const LikeSection = props => {
         className='like-section'
         key='likes-icons-container'
       >
-        <div className='like-section-wrapper'>
+        <div onClick={likePost} className='like-section-wrapper'>
           ❤️
         </div>
         <div className='like-section-wrapper'>
           💬
         </div>
       </div>
-      <p className='like-number'>100 likes</p>
+      <p className='like-number'>{numberOfLikes}</p>
     </div>
   );
 };
